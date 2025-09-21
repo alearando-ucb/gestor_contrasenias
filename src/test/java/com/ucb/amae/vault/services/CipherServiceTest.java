4cd22e61ec84731ab00245dd27c3769564bb64a2
Alejandro Marcelo Arando Espejo <alearandoesp@gmail.com>
2025-09-21 14:30:57 -0400

    test: Se agregaron test para validar el metodo generateIV
    Se agregaron los siguientes test a la clase CipherServiceTest:
    * testGenerateIVReturnsValidIV
    * testGenerateIVValidateUniqueIVs

--- src/test/java/com/ucb/amae/vault/services/CipherServiceTest.java
+++ src/test/java/com/ucb/amae/vault/services/CipherServiceTest.java
@@ -34,4 +34,24 @@ public class CipherServiceTest {
         assertNotNull(salt2);
         assertFalse(java.util.Arrays.equals(salt1, salt2), "Dos salts generados no deberían ser iguales.");
     }
+
+    @Test
+    public void testGenerateIVReturnsValidIV() {
+        // This will fail to compile until we add the generateIV method
+        byte[] iv = cipherService.generateIV();
+
+        assertNotNull(iv, "El IV no debería ser nulo.");
+        assertEquals(12, iv.length, "El IV debería tener una longitud de 12 bytes para AES-GCM.");
+    }
+
+    @Test
+    public void testGenerateIVValidateUniqueIVs() {
+        // This will fail to compile until we add the generateIV method
+        byte[] iv1 = cipherService.generateIV();
+        byte[] iv2 = cipherService.generateIV();
+
+        assertNotNull(iv1);
+        assertNotNull(iv2);
+        assertFalse(java.util.Arrays.equals(iv1, iv2), "Dos IVs generados no deberían ser iguales.");
+    }
 }
