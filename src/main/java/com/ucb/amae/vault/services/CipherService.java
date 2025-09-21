78132d397a5e45a5b4a31c5fc0481629cf7c5eb6
Alejandro Marcelo Arando Espejo <alearandoesp@gmail.com>
2025-09-21 14:25:41 -0400

    feat: Se agrego el metodo generateIV a la clase CipherService

--- src/main/java/com/ucb/amae/vault/services/CipherService.java
+++ src/main/java/com/ucb/amae/vault/services/CipherService.java
@@ -6,6 +6,7 @@ import com.ucb.amae.vault.services.exceptions.CipherException;
 
 public class CipherService {
     public static final int SALT_LENGTH = 16;
+    public static final int IV_LENGTH = 12;
 
     private final SecureRandom secureRandom;
 
@@ -13,13 +14,34 @@ public class CipherService {
         this.secureRandom = new SecureRandom();
     }
 
+    /*
+     * Genera un salt aleatorio de 16 bytes
+     * @return byte[] El salt generado
+     */
     public byte[] generateSalt() {
         try{
             byte[] salt = new byte[SALT_LENGTH];
         secureRandom.nextBytes(salt);
         return salt;
         }catch(Exception e){
-            throw new CipherException("Error generating salt", e);
+            throw new CipherException("Error generando el salt", e);
         }
     }
+
+    /*
+     * Genera un vector de inicialización (IV) aleatorio de 12 bytes
+     * Para usar el algoritmo AES-GCM
+     * @return byte[] El vector de inicialización generado
+     */
+    public byte[] generateIV() {
+        try{
+            byte[] iv = new byte[IV_LENGTH];
+        secureRandom.nextBytes(iv);
+        return iv;
+        }catch(Exception e){
+            throw new CipherException("Error generando el IV", e);
+        }
+    }
+
+
 }
