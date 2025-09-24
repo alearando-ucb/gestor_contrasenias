bcbf4395c8c3fbb05bad538cc68495fdf16449ad
Alejandro Marcelo Arando Espejo <alearandoesp@gmail.com>
2025-09-24 07:07:39 -0400

    feat: Se agrego la funcionalida de mostrar/ocultar contraseña en el componente vault_entry_cell

--- src/main/java/com/ucb/amae/vault/views/components/VaultEntryCellController.java
+++ src/main/java/com/ucb/amae/vault/views/components/VaultEntryCellController.java
@@ -1,10 +1,17 @@
 package com.ucb.amae.vault.views.components;
 
 import com.ucb.amae.vault.model.VaultEntry;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.util.Duration;
 
 public class VaultEntryCellController {
 
@@ -21,6 +28,8 @@ public class VaultEntryCellController {
     @FXML
     private Button togglePasswordVisibilityButton;
     @FXML
+    private ImageView togglePasswordIcon;
+    @FXML
     private Button editEntryButton;
     @FXML
     private Button deleteEntryButton;
@@ -30,27 +39,57 @@ public class VaultEntryCellController {
     private VaultEntry vaultEntry;
     private boolean passwordVisible = false;
 
+    private final Image eyeOpenIcon = new Image(getClass().getResourceAsStream("/com/ucb/amae/vault/icons/eye_open.png"));
+    private final Image eyeClosedIcon = new Image(getClass().getResourceAsStream("/com/ucb/amae/vault/icons/eye_closed.png"));
+
+    @FXML
+    private void initialize() {
+        copyUsernameButton.setOnAction(event -> copyToClipboardWithTimeout(copyUsernameButton, vaultEntry.getUsername()));
+        copyPasswordButton.setOnAction(event -> copyToClipboardWithTimeout(copyPasswordButton, vaultEntry.getPassword()));
+        togglePasswordVisibilityButton.setOnAction(event -> togglePasswordVisibility());
+    }
+
     public void setVaultEntry(VaultEntry entry) {
         this.vaultEntry = entry;
         serviceNameLabel.setText(entry.getServiceName());
         usernameField.setText(entry.getUsername());
         urlField.setText(entry.getUrl());
-        passwordField.setText("********"); // Mask password initially
+        passwordField.setText("********");
         passwordVisible = false;
-        togglePasswordVisibilityButton.setText("Mostrar");
+        togglePasswordIcon.setImage(eyeClosedIcon);
     }
 
-    // Handlers for buttons will be added later in MainVaultController or via callbacks
-    // For now, these methods are just placeholders or will be implemented as part of the cell factory
+    private void copyToClipboardWithTimeout(Button button, String text) {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent content = new ClipboardContent();
+        content.putString(text);
+        clipboard.setContent(content);
 
-    // Example of how a toggle might work (will be connected via MainVaultController)
+        final ImageView originalGraphic = (ImageView) button.getGraphic();
+        button.setGraphic(null);
+        button.setText("¡Copiado!");
+        button.setDisable(true);
+
+        Timeline timeline = new Timeline(
+            new KeyFrame(Duration.seconds(20), ae -> {
+                if (clipboard.hasString() && text.equals(clipboard.getString())) {
+                    clipboard.clear();
+                }
+                button.setText(null);
+                button.setGraphic(originalGraphic);
+                button.setDisable(false);
+            })
+        );
+        timeline.play();
+    }
+
     public void togglePasswordVisibility() {
         if (passwordVisible) {
             passwordField.setText("********");
-            togglePasswordVisibilityButton.setText("Mostrar");
+            togglePasswordIcon.setImage(eyeClosedIcon);
         } else {
             passwordField.setText(vaultEntry.getPassword());
-            togglePasswordVisibilityButton.setText("Ocultar");
+            togglePasswordIcon.setImage(eyeOpenIcon);
         }
         passwordVisible = !passwordVisible;
     }
