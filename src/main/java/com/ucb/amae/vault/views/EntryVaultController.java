6bd3af5b60358b6fc794e3241becac3407dd7b32
Alejandro Marcelo Arando Espejo <alearandoesp@gmail.com>
2025-09-23 20:28:22 -0400

    feat: Se realizo un diseño basico de la interfaz entry_vault

--- src/main/java/com/ucb/amae/vault/views/EntryVaultController.java
+++ src/main/java/com/ucb/amae/vault/views/EntryVaultController.java
@@ -3,11 +3,23 @@ package com.ucb.amae.vault.views;
 import com.ucb.amae.vault.App;
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 
 public class EntryVaultController {
 
     @FXML
-    private void switchToMainVault() throws IOException {
-        App.setRoot("main_vault");
-    }
+    private Label lastVaultNameLabel;
+    @FXML
+    private PasswordField masterPasswordField;
+    @FXML
+    private Button changeVaultButton;
+    @FXML
+    private Button openVaultButton;
+    @FXML
+    private Button createNewVaultButton;
+    @FXML
+    private Label statusLabel;
+
 }
